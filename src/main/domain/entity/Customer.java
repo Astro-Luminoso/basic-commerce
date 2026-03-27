6e123e1d89486fc0db08bbea4e0aed5ab8993402
hanbyeol.yang <beuri97@gmail.com>
2026-03-27 19:33:37 +0900

    feat: add Class Customer

---
+++ src/main/domain/entity/Customer.java
@@ -0,0 +1,16 @@
+package main.domain.entity;
+
+public class Customer {
+
+    private String name;
+    private String email;
+    private String membership;
+
+
+    public Customer (String name, String email, String membership) {
+
+        this.name = name;
+        this.email = email;
+        this.membership = membership;
+    }
+}
