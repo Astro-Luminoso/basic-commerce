cc9844807dc4887ea00b453bd7c5ae25ef3f3da2
hanbyeol.yang <beuri97@gmail.com>
2026-03-27 14:20:54 +0900

    feat: rename package to lowercase and add user input handling

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,15 @@
-package Main;
+package main;
 
-import Main.entity.Product;
+import main.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         List<Product> products = new ArrayList<>(List.of(
                 new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 50),
@@ -15,8 +18,17 @@ public class Main {
                 new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 70)
         ));
 
+        System.out.println("[ 실시간 커머스 플랫 - 전자제품 ]");
+
         for(Product product : products){
             System.out.printf("%d. %s", products.indexOf(product) + 1, product.toString());
+        }
+
+        System.out.println("0. 종료");
+
+        while(true) {
+            int value = sc.nextInt();
+            if(value == 0) break;
         }
     }
 }
--- src/main/entity/Product.java
+++ src/main/entity/Product.java
@@ -1,4 +1,4 @@
-package Main.entity;
+package main.entity;
 
 public class Product {
 
