eea36ff22345710d9ccac84ffa33aefd4117569a
hanbyeol.yang <beuri97@gmail.com>
2026-03-30 22:21:21 +0900

    feat: add new Class IoController.java

---
+++ src/main/service/IoController.java
@@ -0,0 +1,12 @@
+package main.service;
+
+import java.util.Scanner;
+
+public class IoController {
+
+    private final Scanner sc;
+
+    public IoController (Scanner sc) {
+        this.sc = sc;
+    }
+}
