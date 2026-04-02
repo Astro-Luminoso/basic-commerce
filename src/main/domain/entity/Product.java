eacde4e67fb87c558bd8ec891757a539dd8cef10
hanbyeol.yang <beuri97@gmail.com>
2026-04-02 12:14:32 +0900

    docs: add javadoc

--- src/main/domain/entity/Product.java
+++ src/main/domain/entity/Product.java
@@ -3,6 +3,10 @@ package main.domain.entity;
 import main.domain.IterableOptions;
 import main.dto.NewProductDetail;
 
+/**
+ * 제품(Product) 클래스는 제품의 이름, 가격, 설명, 재고 수량을 포함한다.
+ * 제품의 상태를 관리하는 메서드가 포함된다.
+ */
 public class Product implements IterableOptions {
 
 
@@ -46,6 +50,11 @@ public class Product implements IterableOptions {
         this.stockAmount -= amount;
     }
 
+    /**
+     * 재품의 상세정보를 일괄적으로 수정하는 메서드
+     *
+     * @param detail 수정된 제품의 상세정보를 담은 record 형식의 DTO 객체
+     */
     public void update(NewProductDetail detail) {
 
         this.name = detail.name();
@@ -54,6 +63,11 @@ public class Product implements IterableOptions {
         this.stockAmount = detail.stockAmount();
     }
 
+    /**
+     * 괜히 만들었나 싶은 사실 toString() 메서드로 대체 가능한 메서드. 실제로 이 프로그램에서는 toString과 역할이 동일하다.
+     *
+     * @return 상품 객체의 정보를 특정 형식으로 반환하는 문자열
+     */
     @Override
     public String getInfo() {
 
--- src/main/service/AdminAuthentication.java
+++ src/main/service/AdminAuthentication.java
@@ -1,5 +1,8 @@
 package main.service;
 
+/**
+ * 관리자 인증을 관리하는 클래스
+ */
 public class AdminAuthentication {
 
     private final String pwd;
@@ -8,6 +11,12 @@ public class AdminAuthentication {
         this.pwd = pwd;
     }
 
+    /**
+     * 입력된 비밀번호가 관리자 비밀번호와 일치하는지 확인하는 메소드
+     *
+     * @param pwd 사용자가 입력한 비밀번호
+     * @return 동일하면 true, otherwise false
+     */
     public boolean isAuthenticated(String pwd) {
 
         return this.pwd.equals(pwd);
