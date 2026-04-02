e1dac830bb0906a67cd5bbbce230a6fc3366514d
hanbyeol.yang <beuri97@gmail.com>
2026-04-02 13:02:06 +0900

    docs: add javadoc

--- src/main/service/CommerceSystem.java
+++ src/main/service/CommerceSystem.java
@@ -8,6 +8,9 @@ import main.utility.MembershipType;
 
 import java.util.List;
 
+/**
+ * 시스템 데이터 처리를 일괄적으로 관리하는 클래스
+ */
 public class CommerceSystem {
 
     private final List<Category> categories;
@@ -28,13 +31,27 @@ public class CommerceSystem {
         return this.cart;
     }
 
-    private boolean inspectProductDuplication(int index, String productName) {
+    /**
+     * 중복된 상품이 존재하는지 검사하는 메소드
+     *
+     * @param categoryIndex 상품이 추가될 카테고리의 인덱스
+     * @param productName 상품 이름
+     * @return 같은 이름의 상품이 없다면 true, 그 외 false
+     */
+    private boolean inspectProductDuplication(int categoryIndex, String productName) {
 
-        List<Product> products = categories.get(index).getProducts();
+        List<Product> products = categories.get(categoryIndex).getProducts();
 
         return products.stream().noneMatch(product -> product.getName().equals(productName));
     }
 
+    /**
+     * 신규 재품 등록을 처리하는 메소드
+     *
+     * @param index 카테고리 인덱스
+     * @param productDetail 신규상품 상세 정보가 담긴 record 객체
+     * @return 상품이 성공적으로 등록되었다면 true, 그 외 false
+     */
     public boolean addProductDetail(int index, NewProductDetail productDetail) {
 
         boolean isUnique = this.inspectProductDuplication(index, productDetail.name());
@@ -51,11 +68,24 @@ public class CommerceSystem {
         return isUnique;
     }
 
+    /**
+     *  카태고리 인덱스와 상품 인덱스를 받아 해당 상품 객체를 반환하는 메소드
+     *
+     * @param categoryIndex 카테고리 인덱스
+     * @param productIndex 상품 인덱스
+     * @return 상품 객체
+     */
     public Product getProduct(int categoryIndex, int productIndex) {
 
         return this.categories.get(categoryIndex).getProducts().get(productIndex);
     }
 
+    /**
+     * 상품 이름으로 상품 객체를 반환하는 메소드
+     *
+     * @param productName 상품 이름
+     * @return 상품 객체
+     */
     public Product getProduct(String productName) {
 
         return this.categories.stream()
@@ -64,31 +94,58 @@ public class CommerceSystem {
                 .findFirst().orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
     }
 
+    /**
+     * 상품 객체를 받아 카트에 추가하는 메소드
+     * @param product 카트에 추가할 메소드
+     */
     public void addProductToCart(Product product) {
 
         cart.addProduct(product);
     }
 
+    /**
+     * 멤버십 옵션을 받아 카트의 총 가격에 대한 할인된 가격을 반환하는 메소드
+     *
+     * @param membershipOption 선택된 맴버십
+     * @return 할인된 가격
+     */
     public int getDiscountedPrice(int membershipOption) {
         MembershipType membership = MembershipType.values()[--membershipOption];
 
         return membership.calculateDiscountedPrice(cart.getTotalPrice());
     }
 
+    /**
+     * 재고 처리 후 카트 내의 상품들을 모두 제거하는 메소드
+     */
     public void processCheckout() {
         this.cart.getCartList().forEach(product -> product.updateStock(1));
         this.removeOrder();
     }
 
+    /**
+     * 카트 내 상품들을 모두 제거하는 메소드
+     */
     public void removeOrder() {
         this.cart.clearCart();
     }
 
+    /**
+     * 상품 정보를 수정하는 메소드
+     *
+     * @param product 수정 대상 상품 객체
+     * @param updateDetail 수정된 상품의 상세정보가 담긴 record 형식의 DTO 객체
+     */
     public void updateProduct(Product product, NewProductDetail updateDetail) {
 
         product.update(updateDetail);
     }
 
+    /**
+     * 상품객체를 받아 해당 상품이 담긴 카테고리를 찾은 후 카테고리 내에 있는 상품을 찾아 삭제하는 메소드
+     *
+     * @param product 삭제 대상 상품 객체
+     */
     public void removeProduct(Product product) {
 
         this.getCategoriesList().stream()
